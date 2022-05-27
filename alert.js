dd2d6819d3ef41e7e9f829bf132abc8009ec7707
synfm@126.com <synfm@126.com>
2022-05-26 19:39:07 -0500

    Seperate alerts

---
+++ alert.js
@@ -0,0 +1,45 @@
+import { getNicehashAlerts } from './nicehash.js'
+
+var alerts = []
+var trashAlert = {
+  "level": "info",
+  "msg": "Trash will be collected tomorrow"
+}
+
+export function startAlerts(notifyClients) {
+
+  // Check every 5 seconds for all alerts
+  setInterval(function() {
+    let tempAlerts = getNicehashAlerts()
+    if (trashAlert) tempAlerts.push(trashAlert)
+    alerts = tempAlerts.length == 0 ? [] : tempAlerts
+  }, 5000)
+
+  // Send alerts every minutes
+  setInterval(function() {
+    notifyClients({alerts: alerts})
+  }, 6000)
+
+  // Check hourly for trash collection
+  setInterval(function() {
+    let bothWendesday = new Date(1646805600000)
+    let now = new Date()
+
+    if (now.getDay() == 3 && now.getHours() > 12) {
+      const diff = Math.floor(Math.ceil(Math.abs(now - bothWendesday) / 86400000) / 7)
+      if (diff % 2 == 0) {
+        trashAlert = {
+          "level": "info",
+          "msg": "Both trash and recycle will be collected tomorrow"
+        }
+      } else {
+        trashAlert = {
+          "level": "info",
+          "msg": "Trash will be collected tomorrow"
+        }
+      }
+    } else {
+      trashAlert = undefined
+    }
+  }, 3600000)
+}
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import http from 'http'
 import path from 'path'
-import WebSocket, { WebSocketServer } from 'ws'
+import { WebSocketServer } from 'ws'
 import { startNicehash, getNicehashStatus, getNicehashAlerts } from './nicehash.js'
 import { startMyq, getMyqStatus } from './myq.js'
 import { startWeather, getWeather } from './weather.js'
 import { restartMiner, toggleMinorFan } from './smartthings.js'
 import { startServerStatus, getServerStatus } from './server.js'
 import { startStock, getStock } from './stock.js'
+import { startAlerts } from './alert.js'
 
 const app = express()
 const server = http.createServer()
@@ -20,13 +21,11 @@ const production = process.env.PRODUCTION == 'true'
 const clientTimeoutLimit = 180000
 
 
-
 const wss = new WebSocketServer({
   server: server
 })
 
 server.on('request', app)
-
 
 
 wss.on('connection', function connection(client) {
@@ -38,7 +37,6 @@ wss.on('connection', function connection(client) {
 
   client.on('message', function message(data) {
     let message = `${data}`
-    // console.log(`${data}`)
 
     if (message == 'heatbeat') {
       client.heatbeat = new Date()
@@ -46,17 +44,12 @@ wss.on('connection', function connection(client) {
   })
 })
 
-
-
-var checkPoint = {
-  "hash": (Math.random() + 1).toString(36).substring(7)
-}
-
 // startMyq(notifyClients, production)
 startNicehash(notifyClients, production)
 // startWeather(notifyClients, production)
 startServerStatus(notifyClients, production)
 startStock(notifyClients, production)
+startAlerts(notifyClients)
 
 function notifyClients(msg) {
   clients = clients.filter(c => {
@@ -71,46 +64,6 @@ function notifyClients(msg) {
   })
 }
 
-var alerts
-// Check every 5 seconds for alerts
-setInterval(function() {
-  let tempAlerts = getNicehashAlerts()
-  if (alert) tempAlerts.push(alert)
-  alerts = tempAlerts.length == 0 ? undefined : tempAlerts
-}, 5000)
-
-// Check hourly for trash collection
-var alert
-setInterval(function() {
-
-  let bothWendesday = new Date(1646805600000)
-  let now = new Date()
-
-  if (now.getDay() == 3 && now.getHours() > 12) {
-    const diff = Math.floor(Math.ceil(Math.abs(now - bothWendesday) / 86400000) / 7)
-    if (diff % 2 == 0) {
-      alert = {
-        "level": "info",
-        "msg": "Both trash and recycle will be collected tomorrow"
-      }
-    } else {
-      alert = {
-        "level": "info",
-        "msg": "Trash will be collected tomorrow"
-      }
-    }
-  } else {
-    alert = undefined
-  }
-}, 3600000)
-
-app.get('/status', async (req, res) => {
-  if (req.query.checkPoint == undefined || req.query.checkPoint != checkPoint.hash) {
-    res.status(200).json({checkPoint: checkPoint.hash, myq: getMyqStatus(), nh: getNicehashStatus(), weather: getWeather(), server: getServerStatus(), alerts: alerts})
-  } else {
-    res.status(200).json({})
-  }
-})
 
 var turn = true
 
@@ -122,10 +75,4 @@ app.get('/test', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-server.listen(port, function() {
-  // console.log(111)
-  // let a = {abc: 'abc'}
-  // let b = {haha: 'haha'}
-  // let merged = {...a, ...b};
-  // console.log(merged)
-})
+server.listen(port, function() {})
