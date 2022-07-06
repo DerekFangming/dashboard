bc0e2e218046475410ca5480628b9219964a7fdd
synfm@126.com <synfm@126.com>
2022-07-06 11:01:33 -0500

    Automatic fan turning

--- smartthings.js
+++ smartthings.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { getNicehashStatus } from './nicehash.js'
 
 const header = {headers: { Authorization: `Bearer ${process.env.SMARTTHINGS_PAT}`}}
 const turnOn = {
@@ -38,13 +39,30 @@ export function startSmartthings(notifyClients, production) {
     notifyClients(getSmartthingsStatus())
   }, production ? 120000 : 10000 )
 
+  setInterval(function() {
+    let status = getNicehashStatus()
+    let maxTemp = Number.MIN_SAFE_INTEGER
+    for (let d of status.nh.miner.devices) {
+      if (d.temp > maxTemp) maxTemp = d.temp
+    }
+
+    if (maxTemp > Number.MIN_SAFE_INTEGER) {
+      if (fanOn) {
+        if (maxTemp <= 65) { //65
+          toggleMinorFan()
+        }
+      } else {
+        if (maxTemp >= 75) { //75
+          toggleMinorFan()
+        }
+      }
+    }
+  }, production ? 1200000 : 5000 )
+
 }
 
 export function restartMiner() {
-  // Force shut down
   toggelSwitch(5500).then(function() {
-
-    // Start yo
     setTimeout(async function() {
       toggelSwitch(500)
     }, 5000)
