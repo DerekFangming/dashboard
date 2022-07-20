8947cad3f49668a85e59c780861eec245cd60d51
synfm@126.com <synfm@126.com>
2022-07-20 00:25:49 -0500

    Update every 5 minutes

--- helium.js
+++ helium.js
@@ -21,13 +21,13 @@ function getStatus() {
     axios.get(`https://api.helium.io/v1/hotspots//112n1rehHZRo1qxqD9ksd5zzdBAVtJaAMopo6BeWX3tjxpc3zeQd/roles?cursor=` + cursor).then( res => {
       let rawData = res.data.data.sort((a, b) => a - b)
 
-      for (const rd of rawData) {
-        let date = new Date(0)
-        date.setUTCSeconds(rd.time);
+      // for (const rd of rawData) {
+      //   let date = new Date(0)
+      //   date.setUTCSeconds(rd.time);
     
-        let diff = Math.abs(new Date().valueOf() - date.valueOf()) / 36e5
-        console.log(rd.type + ' ' + diff.toFixed(2) + ' hours ago')
-      }
+      //   let diff = Math.abs(new Date().valueOf() - date.valueOf()) / 36e5
+      //   console.log(rd.type + ' ' + diff.toFixed(2) + ' hours ago')
+      // }
 
       if (rawData.length > 6) rawData = rawData.slice(0, 6)
 
@@ -54,5 +54,5 @@ export function startHelium(notifyClients, production) {
   getStatus()
   setInterval(function() {
     getStatus()
-  }, 30000)
+  }, 300000)
 }
