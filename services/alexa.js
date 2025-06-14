bbbe2f604c75b722d5dc81e1a86584ff8e4feb63
Fangming Ning <synfm@126.com>
2025-06-14 13:40:18 -0500

    Turn off HA webhook

--- services/alexa.js
+++ services/alexa.js
@@ -1,7 +1,3 @@
-import axios from "axios"
-import { getDBClient } from './db.js'
-
-var url
 var notifyClientCopy
 
 export function getAlexaStatus() {
@@ -10,26 +6,11 @@ export function getAlexaStatus() {
 
 export async function startAlexa(notifyClients, production) {
   notifyClientCopy = notifyClients
-  
-  const dbClient = getDBClient(production)
-  try {
-    await dbClient.connect()
-    let result = await dbClient.query(`select value from configurations where key = $1`, ['TEMPERATURE_WEBHOOK_URL'])
-    url = result.rows[0].value
-  } catch (e) {
-    addAlert('zillow', 'error', 'Failed to load zillow API leu: ' + e.message, HOUR_MS * 2)
-  } finally {
-    await dbClient.end()
-  }
 }
 
 export function setAlexaCode(code) {
   notifyClientCopy({
     receivedCode: code
   })
-
-  if (code >= 70 && code <= 80) {
-    axios.post(url, {temperature: code})
-  }
 
 }
